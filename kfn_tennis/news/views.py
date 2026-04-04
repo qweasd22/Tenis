ceba1525709cc808fc24453650c7aa1dbdc7343c
School60Main <danila_k0505@mail.ru>
2026-04-04 14:11:44 +0300

    НОвости

--- kfn_tennis/news/views.py
+++ kfn_tennis/news/views.py
@@ -1,14 +1,30 @@
 from django.views.generic import ListView, DetailView
 from .models import News
+from django.db.models import Q
 
 class NewsListView(ListView):
     model = News
     template_name = 'news/news_list.html'
     context_object_name = 'news_list'
-    paginate_by = 6  # по 6 новостей на странице
+    paginate_by = 6
 
     def get_queryset(self):
-        return News.objects.filter(published=True)
+        queryset = News.objects.filter(published=True)
+
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(full_description__icontains=query)
+            )
+
+        return queryset.order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q', '')
+        return context
+
 
 class NewsDetailView(DetailView):
     model = News
